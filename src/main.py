6a713593f4d3d68aac8069d31f64b75931287f79
Doni Ardiansyah <86934377+xdoniardiansyah@users.noreply.github.com>
2026-07-22 08:43:10 +0700

    feat: add URL input

---
+++ src/main.py
@@ -0,0 +1,7 @@
+print("=========================")
+print("Website Inspector CLI")
+print("=========================\n")
+
+user_input = input("Masukkan URL:\n")
+print()
+print(f"URL :\n{user_input}")
