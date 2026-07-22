5af6efe4989ab1f07077eb3ec8f3679e5e4cd407
Doni Ardiansyah <86934377+xdoniardiansyah@users.noreply.github.com>
2026-07-22 09:35:35 +0700

    feat: add URL validation

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ print("=========================")
 print("Website Inspector CLI")
 print("=========================\n")
 
-user_input = input("Masukkan URL:\n")
+clean_input = input("Masukkan URL:\n").strip()
 print()
-print(f"URL :\n{user_input}")
+
+if clean_input == "":
+    print(f"URL tidak boleh kosong.")
+else:
+    print(f"URL :\n{clean_input}")
